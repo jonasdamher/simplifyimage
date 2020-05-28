f02e9a49103319f876b16751793b19022c47f83c
Jonás <jonas.damher@gmail.com>
2020-05-28 17:02:11 +0100

    add handler error class

---
+++ src/core/ResponseHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jonasdamher\Libimagephp\Core;
+
+class ResponseHandler
+{
+	private static array $response = [
+		'valid' => true,
+		'filename' => null,
+		'errors' => []
+	];
+
+	/** 
+	 * Set errors and save in array called $response.
+	 * @param string $message - Error description.
+	 */
+	public function fail(string $message)
+	{
+		if (self::$response['valid']) {
+			self::$response['valid'] = false;
+		}
+		array_push(self::$response['errors'], $message);
+	}
+
+	protected static function filename(string $name)
+	{
+		self::$response['filename'] = $name;
+	}
+
+	protected static function arrayResponse(): array
+	{
+		return self::$response;
+	}
+}
