ec0774f81be285594a9a6c38edd34c54ff724ff8
Jonás <jonas.damher@gmail.com>
2020-06-02 23:58:08 +0100

    update composer.json

--- src/Image.php
+++ src/Image.php
@@ -14,9 +14,9 @@ use Jonasdamher\Simplifyimage\Core\Validate;
  * Allows upload and update one or multiple images. 
  * Allows conversion format image to WEBP, PNG and JPEG.
  * 
- * @package libimagephp
+ * @package simplifyimage
  * @version 0.1
- * @license https://github.com/jonasdamher/libimagephp/blob/master/LICENSE MIT License
+ * @license https://github.com/jonasdamher/simplifyimage/blob/master/LICENSE MIT License
  * 
  * @author Jonás Damián Hernández [jonasdamher]
  */
